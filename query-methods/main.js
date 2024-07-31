80d338f10d3bb78306e397334a5851d17e60441a
Emily Menchu <menchu.lucila@gmail.com>
2024-07-31 13:00:42 -0600

    clase 16

--- query-methods/main.js
+++ query-methods/main.js
@@ -1,13 +1,19 @@
-const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=11&";
-const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?";
-const API_KEY = 'api_key=live_EmLM3MNG3CDNwYPrgXekPclnnI12Hap2i8i8ufe8ZNhe4Sd3ozZDREAP6x8UojUj';
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=12";
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites";
+const API_URL_DELETE_FAVORITES = "https://api.thecatapi.com/v1/favourites/";
+const API_KEY = 'live_EmLM3MNG3CDNwYPrgXekPclnnI12Hap2i8i8ufe8ZNhe4Sd3ozZDREAP6x8UojUj';
 
 const spanError = document.getElementById('michiError')
 const spanErrorFavorite = document.getElementById('michiErrorFavorite')
 
 async function fetchRandomCats(){
   
-        const response = await fetch(API_URL_RANDOM.concat(API_KEY));
+        const response = await fetch(API_URL_RANDOM, {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': API_KEY
+            }
+        });
         const data = await response.json();
         
         const imgContainer = null || document.getElementById("image-container");
@@ -24,18 +30,25 @@ async function fetchRandomCats(){
                 </article>
                 `).join('')}
                 `;
-                
-                imgContainer.innerHTML = images;       
+            
+                imgContainer.innerHTML = images;   
+
+
+                const favoriteButtons = document.querySelectorAll('.favorite-button');
+                favoriteButtons.forEach((button, index) => {
+                    button.addEventListener('click', () => saveFavouriteMichis(data[index].id));
+                });     
         }
 
-        const favoriteButtons = document.querySelectorAll('.favorite-button');
-        favoriteButtons.forEach(button => {
-            button.addEventListener('click', saveFavouriteMichis);
-        });     
 };
 
 async function fetchFavoriteCats(){
-        const response = await fetch(API_URL_FAVORITES.concat(API_KEY));
+        const response = await fetch(API_URL_FAVORITES, {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': API_KEY
+            }
+        });
         const data = await response.json();
         
         const imgContainer = null || document.getElementById("favorite-image-container");
@@ -48,38 +61,65 @@ async function fetchFavoriteCats(){
             let images = `
             ${data.map(cat => `
                 <article>
-                <img src="${cat.image.url}" alt="Foto gatito favorito" class="favorite-cat" id="favorite${++i}">
-                <button class="delete-button" id="delete-button${i}">➖</button>
+                <img src="${cat.image.url}" alt="Foto gatito favorito" class="image" id="favorite${++i}">
+                <button class="delete-button" id="delete-button${i}">-</button>
                 </article>
                 `).join('')}
                 `;
                 
                 imgContainer.innerHTML = images;    
+
+                const deleteButtons = document.querySelectorAll('.delete-button');
+                deleteButtons.forEach((button, index) => {
+                    button.addEventListener('click', () => deleteFavouriteMichis(data[index].id));
+                });     
         }
 };
 
-async function saveFavouriteMichis(){
-    const res = await fetch (API_URL_FAVORITES.concat(API_KEY), {
+async function saveFavouriteMichis(id){
+    const res = await fetch (API_URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'X-API-KEY': API_KEY
         },
         body: JSON.stringify({
-            image_id: '25'
+            image_id: id
         })
     })
 
-    if (response.status !== 200){
-        spanError.innerHTML = "Hubo un MichiError: " + response.status + data.message;
-    } 
+    if (res.status !== 200){
+        spanError.innerHTML = "Hubo un MichiError: " + res.status;
+    } else {
+        fetchFavoriteCats();
+    }
 }
 
+async function deleteFavouriteMichis(id){
+    const urlDelete = API_URL_DELETE_FAVORITES + id;
+    const res = await fetch (urlDelete, {
+        method: 'DELETE',
+        headers: {
+            'X-API-KEY': API_KEY,
+        }
+    });
+
+    if (res.status !== 200){
+        spanErrorFavorite.innerHTML = "Hubo un MichiError: " + res.status;
+    } else {
+        fetchFavoriteCats();
+    }
+}
+
+async function uploadMichiPicture(){
+    const form = document.getElementById('uploadForm');
+    const formData = new FormData(form);
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('reloadImage');
     button.addEventListener('click', fetchRandomCats);
 });
-
 
 
 fetchRandomCats();
